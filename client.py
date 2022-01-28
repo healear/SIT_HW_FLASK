c55b71e591230e8d102be6bf7a90f2e0be46aa82
healear <43036013+healear@users.noreply.github.com>
2022-01-28 13:50:33 +1000

    File_Storage_client_Plus_Dependancy_fix

--- client.py
+++ client.py
@@ -1,29 +1,51 @@
 import requests
 class Client:
     """Инициализированный клиент должен быть авторизирован"""
-    def __init__(self, username:str, password:str):
-        self.url = "http://127.0.0.1:5000/"
+    def __init__(self, url:str, username:str, password:str):
+        self.url = url
+        self.todo_url = "/todo"
+        self.download_url = "/download"
+        self.upload_url = "/uploads"
         self.username = username
         self.password = password
-        payload = {"username": username, "password": password}
-        response = requests.post(self.url+"login", json=payload)
+        payload = {"username": self.username, "password": self.password}
+        response = requests.post(self.url+"/login", json=payload)
         self.token = response.json()['token']
         print("User authenticated, Token: "+self.token)
 
     def get_todos(self):
-        response = requests.get(self.url+"todo", headers={'Authorization': f"Bearer {self.token}"})
+        response = requests.get(self.url+self.todo_url, headers={'Authorization': f"Bearer {self.token}"})
         print(response.json())
 
     def post_todo(self, name):
         payload = {"name": name}
-        response = requests.post(self.url+"todo", json=payload, headers={'Authorization': f"Bearer {self.token}"})
+        response = requests.post(self.url+self.todo_url, json=payload, headers={'Authorization': f"Bearer {self.token}"})
         print(response.json())
 
     def update_todo(self, prev_name, new_name):
         payload = {"name": new_name}
-        response = requests.put(self.url+"todo"+"/"+prev_name, json=payload, headers={'Authorization': f"Bearer {self.token}"})
+        response = requests.put(self.url+self.todo_url+"/"+prev_name, json=payload, headers={'Authorization': f"Bearer {self.token}"})
         print(response.json())
 
     def delete_todo(self, name):
-        response = requests.delete(self.url + "todo" + "/" + name, headers={'Authorization': f"Bearer {self.token}"})
+        response = requests.delete(self.url + self.todo_url + "/" + name, headers={'Authorization': f"Bearer {self.token}"})
         print(response.json())
+
+    def post_update(self, name):
+        files = {'upload': open(f'{name}', 'rb')}
+        response = requests.post(self.url+self.upload_url, files=files)
+        print(response.json())
+
+    def get_updates(self):
+        response = requests.get(self.url+self.upload_url)
+        print(response.json())
+
+    def download_update(self, name):
+        response = requests.get(self.url+self.download_url+"/"+name)
+        with open(name, mode="wb") as download:
+            download.write(response.content)
+        print(response.status_code)
+
+    def delete_update(self, name):
+        response = requests.delete(self.url+self.upload_url+"/"+name)
+        print(response.status_code)
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from client import Client
 if __name__ == '__main__':
+    url = "http://127.0.0.1:5000"
+
     username = "vasya"
-    password = "505555"
-    c = Client(username, password)
-    c.get_todos()
+    password = "50555"
+    c = Client(url=url, username=username, password=password)
+    """c.get_todos()
     c.post_todo("sit")
     c.get_todos()
     c.update_todo("sit", "tis")
     c.get_todos()
     c.delete_todo("tis")
     c.get_todos()
+    c.post_update("template.jpg")
+    c.delete_update("template.jpg")"""
